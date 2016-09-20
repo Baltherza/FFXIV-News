c88c627786c8a1cc8ea9fbb32f189d7b9f000901
Karl Reid <karlr42@gmail.com>
2016-09-21 00:36:08 +0100

    twitter support

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-/*
-  A ping pong bot, whenever you send "ping", it replies "pong".
-*/
+const Discord = require("discord.js");
+const Twitter = require("twitter");
 
-// import the discord.js module
-const Discord = require('discord.js');
 
-// create an instance of a Discord Client, and call it bot
+// discord setup
 const bot = new Discord.Client();
-
-// the token of your bot - https://discordapp.com/developers/applications/me
 const token = process.env.DISCORD_BOT_SECRET;
 
-// the ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted.
-bot.on('ready', () => {
-  console.log('I am ready!');
+// twitter setup
+var twitterClient = new Twitter({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	bearer_token: process.env.TWITTER_BEARER_TOKEN
+});
+
+bot.on("ready", () => {
+	console.log("Bot ready.");
 });
 
 // create an event listener for messages
-bot.on('message', message => {
-  // if the message is "ping",
-  if (message.content === 'ping') {
-  	var lastMsg = 
-  		message.channel.messages.last();
-  	var name = lastMsg.author.username;
-  	console.log(lastMsg);
-    // send "pong" to the same channel.
-    message.channel.sendMessage('pong, '+name);
-  }
+bot.on("message", message => {
+	if (message.content === "!xivtweet") {
+		twitterClient.get("statuses/user_timeline.json?screen_name=FF_XIV_EN&count=1", function(error, tweets, response) {
+			if(error){
+				console.log(error);
+				message.channel.sendMessage("Error when getting tweet :(");
+				throw error;
+			}
+			var tweet = tweets[0];
+			console.log(tweet);
+			message.channel.sendMessage(tweet.text);
+		});
+	}
 });
 
-// log our bot in
+// bot login
 bot.login(token);
